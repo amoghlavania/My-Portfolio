f36550be623c41524308b22c34cc5c9d64c2d049
Amogh Lavania <88672446+amoghlavania@users.noreply.github.com>
2023-05-30 23:03:54 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-//  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script> 
-  var typed = new Typed(".auto", {
-    strings: ["Web Developer", "Web Designer", "An Engineer!"],
-    typeSpeed: 60,
-    backSpeed: 70,
-    loop: true,
-  });
